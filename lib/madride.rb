f5c4455014f153b1c37626f5f9156e26bb9f8aba
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-26 09:56:46 +0200

    Some cleanup

--- lib/madride.rb
+++ lib/madride.rb
@@ -1,10 +1,7 @@
 require "madride/version"
-require "madride/environment"
-require "madride/server"
 
 
 module Madride
   autoload :Environment,  "madride/environment"
   autoload :Server,       "madride/server"
-  autoload :Cli,          "madride/cli"
 end
