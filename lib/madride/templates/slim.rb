97b794d4dd765816ab606db25080d9ca0cbd65db
Aleksey V Zapparov <ixti@member.fsf.org>
2012-08-03 16:49:25 +0200

    Fix SlimTemplate mime type registration

--- lib/madride/templates/slim.rb
+++ lib/madride/templates/slim.rb
@@ -3,9 +3,6 @@ require "slim"
 
 module Madride
   class SlimTemplate < Slim::Template
-    class << self
-      attr_reader :default_mime_type
-      @default_mime_type = 'text/html'
-    end
+    self.default_mime_type = 'text/html'
   end
 end
